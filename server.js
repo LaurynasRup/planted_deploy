15802d964eec1d072d8bff50802f4e64db159d02
Laurynas Rup <lauryfas@gmail.com>
2021-02-08 11:41:17 +0000

    Server typo fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // set static folder
-if (process.env.MODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
